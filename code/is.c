a6120bb93016eb8dd5e9de3476094117dbd87b10
David Beniamine <david.beniamine@imag.fr>
2016-11-22 17:58:31 +0100

    WIP slides

--- code/is.c
+++ code/is.c
@@ -1,4 +1,4 @@
 @|\label{lst:is-sched}@|#pragma omp for schedule(dynamic)
 for( i=0; i< NUM_BUCKETS; i++ )
   for ( k = m; k < bucket_ptrs[i]; k++ )
-    key_buff1[key_buff2[k]]++; @|\label{lst:is-gaus-end}:wq@|
+    key_buff1[key_buff2[k]]++; @|\label{lst:is-gaus-end}@|
